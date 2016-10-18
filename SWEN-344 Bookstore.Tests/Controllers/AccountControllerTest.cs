76a8c22c2596110e80cec0c53fef8af8f13f9c0e
Daniel <dnld1ng@yahoo.com>
2016-10-18 16:27:59 -0400

    Made additional unit test classes

---
+++ SWEN-344 Bookstore.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SWEN_344_Bookstore;
+using SWEN_344_Bookstore.Controllers;
+
+namespace SWEN_344_Bookstore.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        [TestMethod]
+        public void RegisterPage()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Register() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ForgotPassword()
+        {
+            // Arrange
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.ForgotPassword() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
