9d6e5c5ef66a59e2c8eca52b424981743212bb86
ztg6893 <ztg6893@g.rit.edu>
2016-10-18 15:27:18 -0400

    Successfully connected to database

--- SWEN-344 Bookstore/Database/SQLiteDB.cs
+++ SWEN-344 Bookstore/Database/SQLiteDB.cs
@@ -1,38 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Xml.Serialization;
 using System.Data.SQLite;
 
-namespace SWEN_344_Bookstore.Database
+namespace Database_Test
 {
-    public class SQLite_DB
+    class SQLite_Database
     {
+
         public void SQLiteDB()
         {
-            Console.WriteLine("Swen344 SQLite Database");
+            Console.WriteLine("SQLite Database");
 
-            SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Swen344.sqlite;Version=3;");
-            string dbQuery = "SELECT * FROM [dbo].[Table]";
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Swen344.sqlite; Version=3; Integrated Security=True");
 
-            SQLiteCommand command = new SQLiteCommand(dbQuery, connection);
+            string query = "SELECT * FROM [Table] ";
+
+            SQLiteCommand command = new SQLiteCommand(query, connection);
             connection.Open();
-
             SQLiteDataReader reader = command.ExecuteReader();
             try
             {
-                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine(" ----------------------------------------------------");
                 while (reader.Read())
                 {
-                    Console.WriteLine("-----------------------------------------");
-
+                    Console.WriteLine(" ----------------------------------------------------");
                 }
-                Console.WriteLine("-----------------------------------------");
-            
+                Console.WriteLine(" ----------------------------------------------------");
+                //Console.ReadLine();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
+                //Console.ReadLine();
             }
             finally
             {
