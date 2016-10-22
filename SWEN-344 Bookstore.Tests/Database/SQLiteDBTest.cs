9d9a21424813fea40273b9db9475db72b67e1fb2
Brian Picciano <BJPicciano@gmail.com>
2016-10-22 12:38:54 -0400

    created SQLiteDBTest.cs
    stubbed out needed tests

---
+++ SWEN-344 Bookstore.Tests/Database/SQLiteDBTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SWEN_344_Bookstore.Tests.Database {
+    [TestClass]
+    public class SQLiteDBTest {
+        [TestMethod]
+        public void CreateInventoryBook() {
+
+        }
+
+        public void CreateReview () {
+            
+        }
+
+        public void GetInventoryBook () {
+            
+        }
+
+        public void GetReviews () {
+            
+        }
+    }
+}
