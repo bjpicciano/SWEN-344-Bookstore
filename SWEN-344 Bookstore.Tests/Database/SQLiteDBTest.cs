176c296a4363393ae9f899b8baa1bb2d282e4d23
Brian Picciano <BJPicciano@gmail.com>
2016-10-22 12:55:07 -0400

    added BookID column to the InventoryBook table

--- SWEN-344 Bookstore.Tests/Database/SQLiteDBTest.cs
+++ SWEN-344 Bookstore.Tests/Database/SQLiteDBTest.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using Database_Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SWEN_344_Bookstore.Tests.Database {
     [TestClass]
     public class SQLiteDBTest {
+        private SQLite_Database localDB = SQLite_Database.GetInstance();
+
         [TestMethod]
         public void CreateInventoryBook() {
+            localDB.InsertInventoryBook(0, false, 1);
+
+//            var books = localDB.GetInventoryBooks();
 
             Assert.Fail();
         }
