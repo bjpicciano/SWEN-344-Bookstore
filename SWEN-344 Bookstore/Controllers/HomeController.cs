b0f2ca1d4a085e45d9b28939ac55f0fd6441031a
Ryan <Ryan@MOLTENCORE>
2016-11-29 18:27:12 -0500

    Added the catalog, accessible at /Home/Catalog

--- SWEN-344 Bookstore/Controllers/HomeController.cs
+++ SWEN-344 Bookstore/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SWEN_344_Bookstore.Database;
+using SWEN_344_Bookstore.Models;
 
 namespace SWEN_344_Bookstore.Controllers {
     public class HomeController : Controller {
@@ -19,6 +21,29 @@ namespace SWEN_344_Bookstore.Controllers {
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Catalog()
+        {
+            RestAccess ra = RestAccess.GetInstance();
+            IList<Book> books = ra.GetBooks();
+            List<List<Book>> bookTable = new List<List<Book>>();
+            for(int r = 0; r < 5; r++)
+            {
+                bookTable.Add(new List<Book>());
+                if (books.Count > r * 5)
+                {
+                    for (int c = 0; c < 5; c++)
+                    {
+                        if(books.Count > (r * 5) + c)
+                        {
+                            bookTable[r].Add(books.ElementAt((r * 5) + c));
+                        }
+                    }
+                }
+            }
+            ViewData["books"] = bookTable;
             return View();
         }
 
